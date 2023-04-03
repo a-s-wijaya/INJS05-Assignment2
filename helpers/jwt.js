291117f03fcd3aaa2e128bfffa42328c702c5e26
a-s-wijaya <adrian.s.wijaya@upi.edu>
2023-04-03 22:55:26 +0700

    Update jwt.js

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -1,12 +1,12 @@
 const jwt = require("jsonwebtoken");
 const secret = "mysecret";
 
-function sign(user) {
+function generateToken(user) {
   return jwt.sign({ username: user.username, password: user.password }, secret);
 }
 
-function verify(token, callback) {
+function verifyToken(token, callback) {
   return jwt.verify(token, secret, callback);
 }
 
-module.exports = { sign, verify };
+module.exports = { generateToken, verifyToken };
